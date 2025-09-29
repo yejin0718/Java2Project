ac38a345b4e25c873a689523dd7c2009a8f6fb8e
yejin0718 <a89557400@gmail.com>
2025-09-29 16:36:32 +0900

    jdbc connector 연결 중

---
+++ src/jdbc_test/JDBCConnector.java
@@ -0,0 +1,8 @@
+package jdbc_test;
+
+public class JDBCConnector {
+    private static final String DRIVER_PATH = "oracle.jdbc.driver.OracleDriver";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe";
+    private static final String USER_NAME = "";
+    private static final String PASSWORD = "1234";
+}
